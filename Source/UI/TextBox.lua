d045904fa9b10a704ed06a7195573188f2ff6bc5
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2023-01-25 15:56:16 -0800

    Update TextBox.lua

---
+++ Source/UI/TextBox.lua
@@ -0,0 +1,16 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local pd <const> = playdate
+
+class('RigidBody2D').extends(Object)
+
+function TextBox:init()
+    RigidBody2D.super.init(self)
+end
+
+function TextBox:update()
+    RigidBody2D.super.update(seqlf)
+end
