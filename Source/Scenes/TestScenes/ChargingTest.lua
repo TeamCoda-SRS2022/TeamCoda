90250f634c4641807bd694dada40849698309245
Alexander Hwang <ahwang02@ucla.edu>
2023-01-27 19:29:28 -0800

    create skeleton for charging test scene

---
+++ Source/Scenes/TestScenes/ChargingTest.lua
@@ -0,0 +1,18 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "YLib/SceneManagement/Scene"
+import "Player/Player"
+
+local MAX_CHARGE <const> = 144
+
+class('ChargingTest').extends(Scene)
+
+function ChargingTest:init()
+
+end
+
+function ChargingTest:load()
+
+end
