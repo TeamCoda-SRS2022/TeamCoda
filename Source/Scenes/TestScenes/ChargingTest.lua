8f26e35f027cadf711a0420a969359b6d19647d6
Alexander Hwang <ahwang02@ucla.edu>
2023-01-28 22:17:14 -0800

    UI element now synced to charge variable

--- Source/Scenes/TestScenes/ChargingTest.lua
+++ Source/Scenes/TestScenes/ChargingTest.lua
@@ -9,20 +9,23 @@ import "Player/Charging"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+-- Based on current UI element, we want something divisible by 8
+local MAX_CHARGE <const> = 144 
+
+local DEPLETE_RATE <const> = 100
+
 class('ChargingTest').extends(Scene)
 
 function ChargingTest:init()
     ChargingTest.super.init(self)
 
-    self.chargeBar = Charging(144)
+    self.chargeBar = Charging(MAX_CHARGE, DEPLETE_RATE)
 
     self.sceneObjects = {
         self.chargeBar
     }
-    print("Scene Init")
 end
 
 function ChargingTest:load()
     ChargingTest.super.load(self)
-    print("loaded")
 end
